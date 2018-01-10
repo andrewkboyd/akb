6be295893e156a4a9bf19e87b452dcf3325d65f8
Andrew K. Boyd <andrewkboyd@gmail.com>
2018-01-10 12:07:57 -0500

    update prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,26 +1,12 @@
 function fish_prompt
-  set -g __fish_prompt_char »
-
-  # Setup colors
-  set -l normal (set_color normal)
-  set -l cyan (set_color cyan)
-  set -l yellow (set_color yellow)
-  set -l bgreen (set_color -o green)
-  set -l bpurple (set_color -o purple)
-  set -l bmagenta (set_color -o magenta)
-  set -l bred (set_color -o red)
-  set -l bcyan (set_color -o cyan)
-  set -l bwhite (set_color -o white)
-
-  # Bottom
   switch $fish_bind_mode
     case default
-      set -l pcolor $bred
+      set_color red
     case insert
-      set -l pcolor $bgreen
+      set_color green
     case visual
-      set -l pcolor $bmagenta
+      set_color magenta
   end
 
-  echo -n $pcolor$__fish_prompt_char $normal
+  echo -n » (set_color normal)
 end
